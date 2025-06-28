217cd1b5192196a1e0b5defba79fe4666ee24621
mohamed ahmed <mo40580944@gmail.com>
2025-06-27 17:52:57 -0700

    Add project files.

---
+++ ConsoleApp1github/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, W1613orld!");
+        }
+    }
+}
