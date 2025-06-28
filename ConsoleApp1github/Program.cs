00ae42598bdff61a119d0e62930bf145f3491de7
mohamed ahmed <mo40580944@gmail.com>
2025-06-27 18:03:31 -0700

    first push

--- ConsoleApp1github/Program.cs
+++ ConsoleApp1github/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, W1613or43543543543543543ld!");
+            Console.WriteLine("Hello, W1613orld!");
+
         }
     }
 }
